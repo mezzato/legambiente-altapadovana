2fb9e80475d8fcfd0ca1d82c03f16c206bd7a760
mezzato <mezzato@gmail.com>
2024-10-19 19:40:49 +0200

    Update import_yesterdays_data.sh

--- sensorcommunity/grafana/influxdb/import_yesterdays_data.sh
+++ sensorcommunity/grafana/influxdb/import_yesterdays_data.sh
@@ -5,12 +5,12 @@ export WORKPATH=$(readlink -f ./mirror)
 
 export SENSORS_BY_CITY=$(readlink -f ./sensors_by_city.csv)
 
-echo Datum: $DATE
+echo Date: $DATE
 
 mkdir -p $WORKPATH
 
 host archive.sensor.community
-sleep 10
+sleep 2
 
 pushd $WORKPATH
 
@@ -21,38 +21,38 @@ else
     exit 1
 fi
 
-for line in $(cat $SENSORS_BY_CITY); do
+
+IFS=$'\n' # set the Internal Field Separator to newline
+for line in $(sed 1,1d $SENSORS_BY_CITY); do
     IFS=$','
     split=($line)
     unset IFS
+
+    echo $line
     # sensor_id,sensor_type,node,city
     # $split is now a bash array
     sensor_id=${split[0]}
     sensor_type=$(echo "${split[1]}" | sed 's/./\L&/g')
-    # node=${split[2]}
-    # city=${split[3]}
+
     file="${DATE}_${sensor_type}_sensor_${sensor_id}.csv"
 
     echo "Downloading $file"
     curl -O https://archive.sensor.community/$DATE/$file
 
-    if [ -f "$file" ]; then
-        echo "$file found."
-    else
+    if [ ! -f "$file" ]; then
         echo "$file not found."
         exit 1
     fi
 
-    sed 's/;/,/g' $file >tmp_$file
+    sed -i 's/;/,/g' $file
 
     echo "Uploading $file to influxdb"
     influx write -b sensorcommunity \
-        -f tmp_$file \
+        -f $file \
         --header "#constant measurement,particulate" \
         --header "#datatype tag,tag,tag,double,double,dateTime:2006-01-02T15:04:05,double,double,double,double,double,double" \
         --skip-verify
 
-    rm tmp_$file
     rm $file
 
 done
