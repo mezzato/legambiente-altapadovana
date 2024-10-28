b6a30f4473626b71e52eeaac8ba888e7ff5283b2
mezzato <mezzato@gmail.com>
2024-10-28 12:16:17 +0100

    Revert "Add chip_id and city to import"
    This reverts commit c161d73d09c0c5ccb6621ea379f9ebe14e54b691.

--- sensorcommunity/influxdb/import_yesterdays_data.sh
+++ sensorcommunity/influxdb/import_yesterdays_data.sh
@@ -33,8 +33,6 @@ for line in $(sed 1,1d $SENSORS_BY_CITY); do
     # $split is now a bash array
     sensor_id=${split[0]}
     sensor_type=$(echo "${split[1]}" | sed 's/./\L&/g')
-    chip_id=${split[2]}
-    city=${split[3]}
 
     file="${DATE}_${sensor_type}_sensor_${sensor_id}.csv"
 
@@ -51,9 +49,6 @@ for line in $(sed 1,1d $SENSORS_BY_CITY); do
     echo "Uploading $file to influxdb"
     influx write -b sensorcommunity \
         -f $file \
-        --header "#constant measurement,particulate" \        
-        --header "#constant tag,chip_id,$chip_id" \
-        --header "#constant tag,city,$city" \
         --header "#constant measurement,particulate" \
         --header "#datatype tag,tag,tag,double,double,dateTime:2006-01-02T15:04:05,double,double,double,double,double,double" \
         --skip-verify
